d4c5acb8f47830c121bec16955b8cdf06b15c54a
Nada El imache <nadaelimache@gmail.com>
2023-09-13 16:21:59 +0200

    first commit

---
+++ to-do-list.js
@@ -0,0 +1,12 @@
+const contactForm = document.getElementById('container') // salvo un riferimento al form in una variabile
+
+contactForm.addEventListener('submit', function (e) {
+  e.preventDefault()
+  // ora cominciamo a scrivere la NOSTRA logica :)
+  const coffeeInputField = document.getElementById('coffee')
+  const gymInputField = document.getElementById('gym')
+  const studyInputField = document.getElementById('study')
+  const prayerInputField = document.getElementById('pray')
+
+})
+
